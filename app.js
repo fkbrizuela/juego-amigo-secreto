2e1138438dabb995936d4e18a788bdb7039d844f
fkbrizuela <fkbrizuela91@gmail.com>
2025-02-05 23:45:33 -0300

    Agregado array nombres y función para tomar el valor del input

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaAmigos = [];
+
+function agregarAmigo (){
+    let inputNombre = document.getElementById("amigo");
+    let nombre = inputNombre.value;
+    console.log(nombre);
+    
+}
