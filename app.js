ce4ccdf46c3fd5d34034ead76f4a715d482e5936
fkbrizuela <fkbrizuela91@gmail.com>
2025-02-06 21:51:55 -0300

    implementación función agregar elementos a la lista

--- app.js
+++ app.js
@@ -6,5 +6,14 @@ function agregarAmigo (){
     let inputNombre = document.getElementById("amigo");
     let nombre = inputNombre.value;
     console.log(nombre);
-    
+    if (nombre == "") {
+        alert("Por favor ingrese un nombre valido");
+    } else {
+        listaAmigos.push(nombre);
+    }
+    console.log(listaAmigos);
+    inputNombre.value = "";
+    return listaAmigos;
 }
+
+
